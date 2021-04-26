87264b3978797246aa50139be8c1bc93b22c4ea2
Paulo Roberto <paulorobertosp@gmail.com>
2021-04-26 10:55:47 -0300

    Create fail allocation request queue for event

---
+++ src/main/java/br/com/prcompany/beerevents/events/AllocateFailRequest.java
@@ -0,0 +1,16 @@
+package br.com.prcompany.beerevents.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AllocateFailRequest {
+    private UUID uuid;
+}
--- src/main/java/br/com/prcompany/beerevents/utils/EventsConstants.java
+++ src/main/java/br/com/prcompany/beerevents/utils/EventsConstants.java
@@ -8,4 +8,5 @@ public abstract class EventsConstants {
     public static final String VALIDATE_ORDER_RESULT_QUEUE = "validate-order-result";
     public static final String ALLOCATE_ORDER_QUEUE = "allocate-order";
     public static final String ALLOCATE_ORDER_RESULT_QUEUE = "allocate-order-result";
+    public static final String ALLOCATE_FAIL_QUEUE = "allocate-fail";
 }
